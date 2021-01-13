607eaaf04e7d45dbb920bf5e728267a6e3f6a630
ThaiXuanHieu <s2freedom6789@gmail.com>
2021-01-13 22:35:36 +0700

    update ui

--- src/GDStore.MVC/Controllers/HomeController.cs
+++ src/GDStore.MVC/Controllers/HomeController.cs
@@ -37,5 +37,18 @@ namespace GDStore.MVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Policy()
+        {
+            return View();
+        }
     }
 }
